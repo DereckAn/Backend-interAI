ef23a4d0a023bab87e6964a67445c12dd53be1bd
DereckAn <dereckangeles15071998@gmail.com>
2025-03-24 00:21:50 -0600

    Initial commit 2

---
+++ src/test/java/dereck/angeles/practiceIT.java
@@ -0,0 +1,8 @@
+package dereck.angeles;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class practiceIT extends practiceTest {
+    // Execute the same tests but in packaged mode.
+}
